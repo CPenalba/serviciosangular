2e5ec0a3d2ca6ce65d52d5e2f78233e07bab2899
CPenalba <carolinapenalbacorpa@gmail.com>
2024-11-08 13:37:13 +0100

    Padre e hijo comic terminado

--- src/app/components/padre-libreria/padre-libreria.component.ts
+++ src/app/components/padre-libreria/padre-libreria.component.ts
@@ -1,18 +1,27 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Comic } from '../../models/comic';
+import { ServiceComics } from '../../services/service.comics';
 
 @Component({
   selector: 'app-padre-libreria',
   templateUrl: './padre-libreria.component.html',
   styleUrl: './padre-libreria.component.css',
+  //debemos inyectar un servicio para poder recuperarlo posteriormente en el constructor
+  providers: [ServiceComics],
 })
-export class PadreLibreriaComponent {
+export class PadreLibreriaComponent implements OnInit {
   @ViewChild('cajanombre') cajaNombre!: ElementRef;
   @ViewChild('cajaimagen') cajaImagen!: ElementRef;
   @ViewChild('cajadescripcion') cajaDescripcion!: ElementRef;
 
-  public comics: Array<Comic>;
+  public comics!: Array<Comic>;
   public comicFavorito!: Comic;
+
+  constructor(private _service: ServiceComics) {}
+
+  ngOnInit(): void {
+    this.comics = this._service.getComics();
+  }
 
   seleccionarFavorito(event: Comic): void {
     this.comicFavorito = event;
@@ -24,35 +33,5 @@ export class PadreLibreriaComponent {
     let descripcion = this.cajaDescripcion.nativeElement.value;
     let comicNew = new Comic(nombre, imagen, descripcion);
     this.comics.push(comicNew);
-  }
-
-  constructor() {
-    this.comics = [
-      new Comic(
-        'Spiderman',
-        'https://images-na.ssl-images-amazon.com/images/I/61AYfL5069L.jpg',
-        'Hombre araña'
-      ),
-      new Comic(
-        'Wolverine',
-        'https://i.etsystatic.com/9340224/r/il/42f0e1/1667448004/il_570xN.1667448004_sqy0.jpg',
-        'Lobezno'
-      ),
-      new Comic(
-        'Guardianes de la Galaxia',
-        'https://cdn.normacomics.com/media/catalog/product/cache/1/thumbnail/9df78eab33525d08d6e5fb8d27136e95/g/u/guardianes_galaxia_guadianes_infinito.jpg',
-        'Yo soy Groot'
-      ),
-      new Comic(
-        'Avengers',
-        'https://d26lpennugtm8s.cloudfront.net/stores/057/977/products/ma_avengers_01_01-891178138c020318f315132687055371-640-0.jpg',
-        'Los Vengadores'
-      ),
-      new Comic(
-        'Spawn',
-        'https://i.pinimg.com/originals/e1/d8/ff/e1d8ff4aeab5e567798635008fe98ee1.png',
-        'Todd MacFarlane'
-      ),
-    ];
   }
 }
