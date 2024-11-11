de6f99a26dde23e340d924010189efa09729c7af
CPenalba <carolinapenalbacorpa@gmail.com>
2024-11-11 10:01:08 +0100

    Global

--- src/app/services/service.personas.ts
+++ src/app/services/service.personas.ts
@@ -1,25 +1,26 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Global } from '../global';
 
 @Injectable()
 export class ServicePersonas {
   //para las peticiones api viene un objeto llamado HttpClient que nos permitira realizar las peticiones
   constructor(private _http: HttpClient) {}
   getPersonas(): Observable<any> {
-    let urlApiPersonas =
-      'https://servicioapipersonasmvcpgs.azurewebsites.net/api/personas';
+    let request = 'api/personas';
+    let url = Global.urlApiPersonas + request;
     //tenemos dos formas de realizar la funcionalidad de devolucion de datos
     //1)Igual que en vue, creando una promesa por encima de este metodo
     //2)Devolver directamente la peticion para que sea el component quien se subscriba
-    return this._http.get(urlApiPersonas);
+    return this._http.get(url);
   }
 
   getPersonasPromesa(): Promise<any> {
-    let urlApiPersonas =
-      'https://servicioapipersonasmvcpgs.azurewebsites.net/api/personas';
+    let request = 'api/personas';
+    let url = Global.urlApiPersonas + request;
     let promise = new Promise((resolve) => {
-      this._http.get(urlApiPersonas).subscribe((response) => {
+      this._http.get(url).subscribe((response) => {
         resolve(response);
       });
     });
